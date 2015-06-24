910ea0f4af7b79858e35d0f7cd85bc14968d71ba
Ilya Rumyantsev <ilya.rumyantsev@zv-extern.fraunhofer.de>
2015-06-24 16:46:39 +0200

    init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pywave',
+    version='0.1',
+    packages=find_packages(),
+    url='http://rumyantsev.de',
+    license='MIT',
+    author='Ilya Rumyantsev',
+    author_email='contact@rumyantsev.de',
+    description='python api for the zway project'
+)
